4bc979ed8863f033d1faf7d81953cd3f08b17e9b
Zulkaif Khan <zulkaifkhan183@gmail.com>
2026-01-02 15:35:55 +0500

    custom map function

---
+++ Custom Map/script.js
@@ -0,0 +1,19 @@
+// Custom Utilities (how to make your own Map function or method);
+
+
+function myMap(array , callback){
+    let newArray = [];
+
+    for(let i = 0 ; i < array.length ; i++){
+        newArray.push(callback(array[i]));
+    }
+
+    return newArray ;
+}
+
+let array = [1,2,3];
+let ans = myMap(array,function(arr){
+    return arr + 1 ;
+})
+
+console.log(ans)
